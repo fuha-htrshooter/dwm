a1837b07f58d0eb09133ac7bceccc3edb76c5527
Tatsuya Yanagisawa <nrd15078@nifty.com>
2021-03-26 20:49:01 +0900

    公開用初コミット

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 pkgname=dwm-git
 _pkgname=dwm
-pkgver=6.2.r0.gcb3f58a
+pkgver=6.2.r8.g61bb8b2
 pkgrel=1
 pkgdesc="A dynamic window manager for X"
 url="http://dwm.suckless.org"
@@ -14,10 +14,27 @@ provides=('dwm')
 conflicts=('dwm')
 source=(dwm.desktop
         "$_pkgname::git+http://git.suckless.org/dwm"
-        config.h)
+        config.h
+        https://dwm.suckless.org/patches/awesomebar/dwm-awesomebar-20191003-80e2a76.diff
+        https://dwm.suckless.org/patches/gridmode/dwm-gridmode-20170909-ceac8c9.diff
+        https://dwm.suckless.org/patches/statusallmons/dwm-statusallmons-20160731-56a31dc.diff
+        https://dwm.suckless.org/patches/attachbelow/dwm-attachbelow-6.2.diff
+        https://dwm.suckless.org/patches/cursorwarp/dwm-cursorwarp-mononly-20210222-61bb8b2.diff
+        awesomebar-multimons.diff
+        dwm-extrabar-6.2-rebuild.diff
+        dwm-statusmonsbutton.patch)
+# so you can customize config.h and local-patches
 md5sums=('939f403a71b6e85261d09fc3412269ee'
          'SKIP'
-         'SKIP') # so you can customize config.h
+         'SKIP'
+         '5754406d68c49ca6db158b9569f45de7'
+         '6055775113fd4dc06200bc6aaafb72fb'
+         'f3cd9551364f69f247eacf0c4b15d885'
+         '4d221bca8b074097748414029f175459'
+         '25b1b39ba1f8840ed3f80c4f72ca5cb0'
+         'SKIP'
+         'SKIP'
+         'SKIP')
 
 pkgver(){
   cd $_pkgname
@@ -29,6 +46,27 @@ prepare() {
   if [[ -f "$srcdir/config.h" ]]; then
     cp -fv "$srcdir/config.h" config.h
   fi
+
+  # remove old .rej
+  find $srcdir/ -name '*.rej' | xargs rm
+
+  # awesomebar
+  patch -p1 -i "$srcdir/dwm-awesomebar-20191003-80e2a76.diff"
+  patch -p1 -i "$srcdir/awesomebar-multimons.diff"
+  # cursorwarp
+  patch -p1 -i "$srcdir/dwm-cursorwarp-mononly-20210222-61bb8b2.diff"
+  # attachbelow
+  patch -p1 -i "$srcdir/dwm-attachbelow-6.2.diff"
+  # gridmode
+  rm -fv layouts.c # remove old
+  patch -p1 -i "$srcdir/dwm-gridmode-20170909-ceac8c9.diff"
+  # statusallmons
+  patch -p1 -i "$srcdir/dwm-statusallmons-20160731-56a31dc.diff"
+  # extrabar
+  patch -p1 -i "$srcdir/dwm-extrabar-6.2-rebuild.diff"
+  # statusmonsbutton
+  patch -p1 -i "$srcdir/dwm-statusmonsbutton.patch"
+
 }
 
 build() {
--- config.h
+++ config.h
@@ -5,21 +5,24 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char statussep         = ';';
+static const char stmbtn_sep        = '|';
+static const char *fonts[]          = { "IPAMincho:size=13" };
+static const char dmenufont[]       = "IPAMincho:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_white[]       = "#ffffff";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_white, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_white, col_cyan,  col_cyan  },
+	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,19 +31,21 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 
+#include "layouts.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "HHH",      grid },
 };
 
 /* key definitions */
@@ -56,8 +61,24 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_white, NULL };
+static const char *termcmd[]  = { "mate-terminal", "--hide-menubar", NULL };
+
+enum { StbtnDate = 0, StbtnVolume = 6, StbtnEnd };
+static const char *stmbtn_cmds[][StbtnEnd][9] = {
+	{ /* Button1 (Left) */
+		[StbtnDate] = { "caja", NULL },
+		[StbtnVolume] = { "/bin/sh", "-c", "/home/fuha/bin/volume-down", NULL },
+	},
+	{ /* Button2 (LeftUp) */
+		[StbtnDate] = { "mate-terminal", "--hide-menubar", NULL },
+		[StbtnVolume] = { "/bin/sh", "-c", "/home/fuha/bin/volume-set 100", NULL },
+	},
+	{ /* Button3 (Right) */
+		[StbtnDate] = { "mate-system-monitor", NULL },
+		[StbtnVolume] = { "/bin/sh", "-c", "/home/fuha/bin/volume-up", NULL },
+	}
+};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -76,6 +97,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -100,10 +122,15 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {.v = &layouts[0]} },
+	{ ClkLtSymbol,          0,              Button2,        setlayout,      {.v = &layouts[3]} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button1,        togglewin,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkWinTitle,          0,              Button3,        killclient,     {0} },
+	{ ClkStatusText,        0,              Button1,        stmbutton,      {.v = stmbtn_cmds[0] } },
+	{ ClkStatusText,        0,              Button2,        stmbutton,      {.v = stmbtn_cmds[1] } },
+	{ ClkStatusText,        0,              Button3,        stmbutton,      {.v = stmbtn_cmds[2] } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -112,4 +139,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
